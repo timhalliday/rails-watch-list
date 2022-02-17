6b05b765bea00f4a59c0fea2938da5df8b496f0b
Tim Halliday <thalliday793@gmail.com>
2022-02-17 14:37:23 +0000

    db seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+puts 'Generating movies'
+
+movies['results'].first(20).each do |movie|
+  new_movie = Movie.new(
+                        title: movie['title'],
+                        overview: movie['overview'],
+                        poster_url: movie['poster_path'],
+                        rating: movie['vote_average']
+                       )
+  puts new_movie['title']
+  new_movie.save
+end
